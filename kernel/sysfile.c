6f800a1dd137d7635b1f0088eba3eaf734dae23f
spark2027 <tejaswini.rsuresh@gmail.com>
2026-09-24 04:56:43 +0000

    feat: enforce per-process file descriptor quota

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -42,7 +42,7 @@ fdalloc(struct file *f)
   int fd;
   struct proc *p = myproc();
 
-  for (fd = 0; fd < NOFILE; fd++) {
+  for (fd = 0; fd < MAX_OPEN_FDS; fd++) {
     if (p->ofile[fd] == 0) {
       p->ofile[fd] = f;
       return fd;
